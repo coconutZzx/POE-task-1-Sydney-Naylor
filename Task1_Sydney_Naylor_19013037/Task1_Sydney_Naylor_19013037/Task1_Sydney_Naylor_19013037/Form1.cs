404d98ecb930a7bb62d28a302a48d56b14a6950d
coconutZzx <naylorsydney@gmail.com>
2019-10-07 10:48:19 +0200

    Update Form1.cs

--- Task1_Sydney_Naylor_19013037/Task1_Sydney_Naylor_19013037/Task1_Sydney_Naylor_19013037/Form1.cs
+++ Task1_Sydney_Naylor_19013037/Task1_Sydney_Naylor_19013037/Task1_Sydney_Naylor_19013037/Form1.cs
@@ -12,36 +12,84 @@ namespace Task1_Sydney_Naylor_19013037
 {
     public partial class frmGame : Form
     {
-        Map map = new Map(5);
+        GameEngine engine = new GameEngine();
+        Timer timer = new Timer();
+        GameState gameState = GameState.PAUSED;
+
         public frmGame()
         {
             InitializeComponent();
-            map.GenerateBattlefield();
-            rTxtBox.Text = map.GetInfo();
-            //for (int i = 0; i < map.numberUnits ; i++)
-            //{
-            //    txtBox.Text += map.units[i].ToString();
-            //}
+
+            engine = new GameEngine();
+            lblArea.Text = engine.GetMapDisplay();
+            rTxtBox.Text = engine.GetUnitInfo() + engine.GetBuildingInfo();
+            //rTxtBox.Text = engine.GetBuildingInfo();
+            lblRounds.Text = "Round: " + engine.Round;
+
+            timer = new Timer();
+            timer.Interval = 1000/60;
+            timer.Tick += LblRounds_Click;
             
         }
-
+        private void UpdateUI()
+        {
+            lblArea.Text = engine.GetMapDisplay();
+            rTxtBox.Text = engine.GetUnitInfo() + engine.GetBuildingInfo();
+            //rTxtBox.Text = engine.GetBuildingInfo();
+            lblRounds.Text = "Round: " + engine.Round;
+        }
+        public enum GameState
+        {
+            RUNNING,
+            PAUSED,
+            ENDED
+        }
         private void LblRounds_Click(object sender, EventArgs e)
         {
+            engine.GameLoop();
+            UpdateUI();
 
+            if (engine.GameOver)
+            {
+                timer.Stop();
+                lblArea.Text = engine.WinningTeam + " WON! \n" + lblArea.Text;
+                gameState = GameState.ENDED;
+                btnStartPause.Text = "Restart";
+            }
         }
 
-        private void BtnStart_Click(object sender, EventArgs e)
+        private void BtnStartPause_Click(object sender, EventArgs e)
         {
-            map.Populate();
-            map.Render(lblArea);
 
-            
+            if (gameState == GameState.RUNNING)
+            {
+                timer.Stop();
+                gameState = GameState.PAUSED;
+                btnStartPause.Text = "Start";
+            }
+            else
+            {
+                if (gameState == GameState.ENDED)
+                {
+                    engine.Reset();
+                }
+                timer.Start();
+                gameState = GameState.RUNNING;
+                btnStartPause.Text = "Pause";
+            }
+
+            // Populate();
+            // map.Render(lblArea);
             // map.GenerateBattlefield();
             // map.Populate();
             // map.Update();
             // map.Information(txtBox);
         }
-
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            engine.SaveUnitsToFile();
+            engine.SaveBuidlingsToFile();
+        }
         private void TxtBox_TextChanged(object sender, EventArgs e)
         {
 
